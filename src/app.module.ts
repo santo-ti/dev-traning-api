cfbb144b1d0534ad107f6865ee5e4204d072735b
Anderson Santo <12055277+santo-ti@users.noreply.github.com>
2023-08-27 20:18:54 -0300

    📦 NEW: Project clean

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { CoursesController } from './courses/courses.controller';
+import { CoursesModule } from './courses/courses.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, CoursesController],
-  providers: [AppService],
+  imports: [CoursesModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
