b7822ccb2bdcee009b17a8edac526ae0617e02f2
Anderson Santo <12055277+santo-ti@users.noreply.github.com>
2023-09-05 20:50:34 -0300

    📦 FEAT: Add typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CoursesModule } from './courses/courses.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './configs/typeorm.config';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [CoursesModule],
+  imports: [CoursesModule, TypeOrmModule.forRoot(typeOrmConfig), UsersModule],
   controllers: [],
   providers: [],
 })
