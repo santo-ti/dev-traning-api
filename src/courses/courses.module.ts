60d3b316142500b18ee1d56a3c6353e160d46449
Anderson Santo <12055277+santo-ti@users.noreply.github.com>
2023-09-05 21:30:30 -0300

    👌 REFACTOR: Alter courses folders

--- src/courses/courses.module.ts
+++ src/courses/courses.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { CoursesService } from './courses.service';
-import { CoursesController } from './courses.controller';
+import { CoursesController } from './controllers/courses.controller';
+import { CoursesService } from './services/courses.service';
 
 @Module({
   controllers: [CoursesController],
-  providers: [CoursesService]
+  providers: [CoursesService],
 })
 export class CoursesModule {}
